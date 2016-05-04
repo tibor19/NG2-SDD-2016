e628848e101e83add6e172085adabc33682a48d6
Tiberiu Covaci <tibi@covaci.se>
2016-05-04 22:21:33 +0100

    Add second component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {SecondComponent} from './second.component'
 
 @Component({
     selector : 'recipe-app',
-    templateUrl : './app/app.component.html' 
+    templateUrl : './app/app.component.html',
+    directives: [SecondComponent] 
 })
 export class AppComponent {
     
---
+++ app/second.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector : 'second-cmp',
+    template : '<h2>Second component</h2>' 
+})
+export class SecondComponent{
+    
+}
