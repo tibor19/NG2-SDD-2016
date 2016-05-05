7f1b8775a56a6a2e6a8fe38b730c929f12b24835
Tiberiu Covaci <tibi@covaci.se>
2016-05-05 22:52:04 +0100

    Add component template

--- app/recipe/weekly-menu.component.ts
+++ app/recipe/weekly-menu.component.ts
@@ -5,5 +5,22 @@ import {Component} from 'angular2/core';
     templateUrl : './app/recipe/weekly-menu.component.html' 
 })
 export class WeeklyMenuComponent{
-    title: 'Weekly Menu';
+    title = 'Weekly Menu';
+    recipes = [{
+        "recipeId": 1,
+        "name": "Fish sticks and Rice",
+        "price": 2.0,
+        "time": 20,
+        "image": "fishsticks-mine",
+        "recipeIngredients": [],
+        "recipeInstructions": []
+    }, {
+        "recipeId": 2,
+        "name": "Pizza",
+        "price": 13.0,
+        "time": 30,
+        "image": "pizza-clip",
+        "recipeIngredients": [],
+        "recipeInstructions": []
+    }];
 }
