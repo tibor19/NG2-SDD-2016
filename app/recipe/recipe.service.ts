8b9bfb575bf64e3fdcf36baf7da7c240728fe034
Tiberiu Covaci <tibi@covaci.se>
2016-05-05 23:24:58 +0100

    Add recipe service Move IRecipe definition to the service

---
+++ app/recipe/recipe.service.ts
@@ -0,0 +1,36 @@
+export interface IRecipe {
+    recipeId: number;
+    name: string;
+    price: number;
+    time: number;
+    image: string;
+    dayOfWeek: string;
+}
+
+export class RecipeService {
+    
+    getRecipes() : IRecipe[] {
+        return [{
+            "recipeId": 1,
+            "name": "Fish sticks and Rice",
+            "price": 2.0,
+            "time": 20,
+            "image": "fishsticks-mine",
+            "dayOfWeek": ""
+        }, {
+            "recipeId": 2,
+            "name": "Pizza",
+            "price": 13.0,
+            "time": 30,
+            "image": "pizza-clip",
+            "dayOfWeek": ""
+        }, {
+            "recipeId": 3,
+            "name": "Pasta with Meatsauce",
+            "price": 3.0,
+            "time": 11,
+            "image": "pasta-clip",
+            "dayOfWeek": ""
+        }];
+    }
+}
--- app/recipe/weekly-menu.component.ts
+++ app/recipe/weekly-menu.component.ts
@@ -1,37 +1,29 @@
-import {Component} from 'angular2/core';
-
-interface IRecipe {
-    recipeId: number;
-    name: string;
-    price: number;
-    time: number;
-    image: string;
-    dayOfWeek: string;
-}
+import {Component, OnInit} from 'angular2/core';
+import {IRecipe, RecipeService} from './recipe.service';
 
 @Component({
     selector: 'weekly-menu',
     templateUrl : './app/recipe/weekly-menu.component.html' 
 })
-export class WeeklyMenuComponent{
+export class WeeklyMenuComponent implements OnInit {
     private daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     title = 'Weekly Menu';
-    recipes : IRecipe[] = [{
-        "recipeId": 1,
-        "name": "Fish sticks and Rice",
-        "price": 2.0,
-        "time": 20,
-        "image": "fishsticks-mine",
-        "dayOfWeek": ""
-    }, {
-        "recipeId": 2,
-        "name": "Pizza",
-        "price": 13.0,
-        "time": 30,
-        "image": "pizza-clip",
-        "dayOfWeek": ""
-    }].map((r, i) => {
-        r.dayOfWeek = this.daysOfWeek[i%this.daysOfWeek.length];
-        return r;
-    });
+    recipes : IRecipe[];
+    
+    recipeService: RecipeService;
+    
+    /**
+     *
+     */
+    constructor() {
+        this.recipeService = new RecipeService();
+    }
+    
+    ngOnInit(){
+        this.recipes = this.recipeService.getRecipes().map((r, i) => {
+            r.dayOfWeek = this.daysOfWeek[i%this.daysOfWeek.length];
+            return r;
+        });
+
+    }
 }
