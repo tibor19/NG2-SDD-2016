6a107f39d487d8784b8cc9f79cd4a7c50fddd7d5
Tiberiu Covaci <tibi@covaci.se>
2016-05-05 22:29:48 +0100

    Move the style to the component

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,19 @@ import {WeeklyMenuComponent} from './recipe/weekly-menu.component';
 @Component({
     selector : 'recipe-app',
     templateUrl : './app/app.component.html',
-    directives: [WeeklyMenuComponent]
+    directives: [WeeklyMenuComponent],
+    styles:[`
+        .container {
+            width: 620px;
+            margin: 0 auto;
+            padding: 10px;
+            border-right: 2px solid #725972;
+            border-bottom: 2px solid #725972;
+            border-left: 2px solid #725972;
+            background-color: #fff;
+            margin-top: 10px;
+        }
+    `] 
 })
 export class AppComponent {
     
