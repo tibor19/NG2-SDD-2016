c7b9739acd92ad7b201a98a723c903f2451a2d60
Tiberiu Covaci <tibi@covaci.se>
2016-05-06 00:33:51 +0100

    Add links to main page Add navigation in code to previous page

--- app/recipe/recipe.component.ts
+++ app/recipe/recipe.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from 'angular2/core';
+import {Router} from 'angular2/router';
+
 import {IRecipe, RecipeService} from './recipe.service';
 import {ImagePipeTransform} from '../pipes/image';
 
@@ -9,10 +11,14 @@ import {ImagePipeTransform} from '../pipes/image';
 export class RecipeComponent implements OnInit {
     recipe: IRecipe;
     
-    constructor(private recipeService: RecipeService) {
+    constructor(private recipeService: RecipeService, private router: Router) {
     }
 
     ngOnInit(){
         this.recipe = this.recipeService.getRecipe(1);        
+    }
+    
+    goBack(){
+        this.router.navigate(['WeeklyMenu']);
     }
 }
