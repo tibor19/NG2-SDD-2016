373f2979fed1607378b9303b1e1c03425215942a
Tiberiu Covaci <tibi@covaci.se>
2016-05-05 22:56:35 +0100

    Add strong type info

--- app/recipe/weekly-menu.component.ts
+++ app/recipe/weekly-menu.component.ts
@@ -1,12 +1,22 @@
 import {Component} from 'angular2/core';
 
+interface IRecipe {
+    recipeId: number;
+    name: string;
+    price: number;
+    time: number;
+    image: string;
+    recipeIngredients: any[];
+    recipeInstructions: any[];
+}
+
 @Component({
     selector: 'weekly-menu',
     templateUrl : './app/recipe/weekly-menu.component.html' 
 })
 export class WeeklyMenuComponent{
     title = 'Weekly Menu';
-    recipes = [{
+    recipes : IRecipe[] = [{
         "recipeId": 1,
         "name": "Fish sticks and Rice",
         "price": 2.0,
