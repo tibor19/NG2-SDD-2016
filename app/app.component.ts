98073c379ee8352a4eeac234eb812893a2221911
Tiberiu Covaci <tibi@covaci.se>
2016-05-16 12:27:16 +0100

    Added static weekly menu

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
-import {SecondComponent} from './second.component';
+import {WeeklyMenuComponent} from './recipe/weekly-menu.component';
 
 @Component({
     moduleId: module.id,
     selector : 'recipe-app',
     templateUrl : 'app.component.html',
-    directives: [SecondComponent] 
+    directives: [WeeklyMenuComponent],
+    styleUrls: ['/content/styles.css'] 
 })
 export class AppComponent {
     
---
+++ app/recipe/weekly-menu.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'weekly-menu',
+    templateUrl: 'weekly-menu.component.html'    
+})
+export class WeeklyMenuComponent {
+    
+}
