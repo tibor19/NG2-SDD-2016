c36c4475e4436d8b9aaec4995e3685bdd77a4257
Tiberiu Covaci <tibi@covaci.se>
2016-05-16 12:53:45 +0100

    Fixed the styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,11 @@ import {Component} from 'angular2/core';
 import {WeeklyMenuComponent} from './recipe/weekly-menu.component';
 
 @Component({
+    moduleId: module.id,
     selector : 'recipe-app',
-    templateUrl : './app/app.component.html',
+    templateUrl : 'app.component.html',
     directives: [WeeklyMenuComponent],
-    styleUrls: ['./content/styles.css'] 
+    styleUrls: ['app.component.css'] 
 })
 export class AppComponent {
     
--- app/second.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector : 'second-cmp',
-    template : '<h2>Second component</h2>' 
-})
-export class SecondComponent{
-    
-}
