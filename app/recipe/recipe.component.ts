062f39fec106389a26396bb57818dc567d00055b
Tiberiu Covaci <tibi@covaci.se>
2016-05-16 15:36:51 +0100

    Getting the routing parameters

--- app/recipe/recipe.component.ts
+++ app/recipe/recipe.component.ts
@@ -1,9 +1,24 @@
 import {Component} from 'angular2/core';
+import {OnActivate, ComponentInstruction, RouteParams} from 'angular2/router';
 
 @Component({
     moduleId: module.id,
     templateUrl : 'recipe.component.html'
 })
 export class RecipeComponent{
+    
+    constructor(private routeParams: RouteParams){
+        console.log(routeParams.params['id']);
+    }
+    
+    routerOnActivate(nextInstruction : ComponentInstruction, prevInstruction: ComponentInstruction){
+        let recipeId = parseInt(nextInstruction.params['id']);
+        
+        if(recipeId == -1){
+            recipeId = Math.floor(Math.random() * 8) + 1;
+        }
+        
+        // Get the recipe
+    }
     
 }
